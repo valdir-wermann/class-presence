1856f70403b5170d62c75675f88b75db83df2259
Dinho <valdirwermannjr@gmail.com>
2023-11-06 22:03:31 -0300

    implentamos modificar descricao e periodos

--- aluno/js/modify.js
+++ aluno/js/modify.js
@@ -1,45 +1,61 @@
 const modifySelector = document.querySelector(`#info-select`);
 const tipoForm = document.querySelector(`#tipo-form`);
 const dataForm = document.querySelector(`#data-form`);
+const descForm = document.querySelector('#desc-form');
+const periodsForm = document.querySelector('#periods-form');
+
+const modifyDateInput = document.querySelector('#date-input');
+const modifyDescInput = document.querySelector('#desc-input');
+const modifyPeriodsInput = document.querySelector('#periods-input');
 
 const modifyDateBtn = document.querySelector('#modify-date-btn');
-const modifyDateInput = document.querySelector('#date-input');
+const modifyDescBtn = document.querySelector('#modify-desc-btn');
+const modifyPeriodsBtn = document.querySelector('#modify-periods-btn');
+
 
 modifySelector.addEventListener('change', function(e) {
+    [tipoForm, dataForm, descForm, periodsForm].forEach(form => form.style.display = 'none');
     if (e.target.value === 'tipo')  {
-        console.log(1)
         tipoForm.style.display = 'flex';
-        dataForm.style.display = 'none';
     } else if (e.target.value === 'date') {
-        tipoForm.style.display = 'none';
         dataForm.style.display = 'flex';
-    }
-})
-
-modifyDateBtn.addEventListener('click', function(e) {
-    if(modifyDateInput.value === '') {
-        alert('Por favor, insira uma data válida!')
-    } else {
-        fetch(`https://class-presence-backend.onrender.com/api/attendances/${attendanceId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem('authorization')
-            },
-            body: JSON.stringify({ date: modifyDateInput.value })
-        })
-      .then(res => {
-        if (res.ok) return res.json();
-        if (res.status === 401 || res.status === 403) {
-            alert('Você não tem acesso a essa ação. Redirecionando para página de login.');
-            localStorage.clear();
-            window.location.href = `${window.location.origin}/class-presence/login`;
-        }
-        throw new Error(res);
-    })
-    .then(() => {
-        alert('Presença modificada com sucesso!');
-        window.location.reload();
-    })
+    } else if (e.target.value === 'desc') {
+        descForm.style.display = 'flex';
+    } else if (e.target.value === 'periods') {
+        periodsForm.style.display = 'flex';
     }
 });
+
+const modifyFunction = (input, name) => {
+        if(input.value === '') {
+            alert('Por favor, insira um valor válido!')
+        } else {
+            let body = {};
+            body[name] = input.value;
+            fetch(`https://class-presence-backend.onrender.com/api/attendances/${attendanceId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('authorization')
+                },
+                body: JSON.stringify(body)
+            })
+          .then(res => {
+            if (res.ok) return res.json();
+            if (res.status === 401 || res.status === 403) {
+                alert('Você não tem acesso a essa ação. Redirecionando para página de login.');
+                localStorage.clear();
+                window.location.href = `${window.location.origin}/class-presence/login`;
+            }
+            throw new Error(res);
+        })
+        .then(() => {
+            alert('Presença modificada com sucesso!');
+            window.location.reload();
+        })
+        }
+    }
+
+modifyDateBtn.addEventListener('click', () => modifyFunction(modifyDateInput, 'date'));
+modifyDescBtn.addEventListener('click', () => modifyFunction(modifyDescInput, 'desc'));
+modifyPeriodsBtn.addEventListener('click', () => modifyFunction(modifyPeriodsInput, 'periods'));
